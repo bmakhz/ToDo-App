1a639f03dfcbaf4c35ab16d1166cfa67ce650e9d
bmakhz <bharat.manek04@gmail.com>
2025-07-03 18:03:45 +0530

    Updated page.js
    formatted layout. added shadowed ,rounded columns.

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,19 @@
 
 export default function Home() {
   return (
-    <main >
-      <h1>Todo App</h1>
-      <div>
-        <div>
-          <h2>Pending</h2>
-          {/* Pending */}
+    <main className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold mb-6 text-black">Todo App</h1>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-white rounded-lg shadow p-4">
+          <h2 className="text-xl font-semibold mb-2 text-black">Pending</h2>
+          {/* Pending*/}
         </div>
-        <div>
-          <h2>In Progress</h2>
-          {/* In Progress */}
+        <div className="bg-white rounded-lg shadow p-4">
+          <h2 className="text-xl font-semibold mb-2 text-black">In Progress</h2>
+          {/* In Progress*/}
         </div>
-        <div>
-          <h2>Done</h2>
+        <div className="bg-white rounded-lg shadow p-4">
+          <h2 className="text-xl font-semibold mb-2 text-black">Done</h2>
           {/* Done*/}
         </div>
       </div>
