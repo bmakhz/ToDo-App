3c2f27a5213232c0b473a1c173b9aa8df3c5c3f6
bmakhz <bharat.manek04@gmail.com>
2025-07-03 18:56:09 +0530

    created use.js
    created hook for storage to manage state and save the todo tasks.

---
+++ hooks/use.js
@@ -0,0 +1,37 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export default function useTodos() {
+    
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    const stored = localStorage.getItem('todos');
+    if (stored) setTodos(JSON.parse(stored));
+  }, []);
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const add = (title, description) => {
+    const newTodo = {
+      id: Date.now(),
+      title,
+      description,
+      status: 'Pending',
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const update = (id, status) => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? { ...todo, status } : todo
+    ));
+  };
+
+  const delete = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return { todos, add, update, delete };
+}
